0510aca9ceecd8e7d83532a1eca3fb7c46481906
Nick Friskel <friskelglass@gmail.com>
2016-11-26 15:23:24 -0700

    2.21 on 5x 11/26/16

--- app/src/main/java/com/probationbuddy/probationbuddy/MainActivity.java
+++ app/src/main/java/com/probationbuddy/probationbuddy/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3); //set activity layout UPDATE TEST
+        setContentView(R.layout.activity_main); //set activity layout UPDATE TEST
 
         //set toolbar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_top);
