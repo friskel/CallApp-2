90072ae46356db4278608d5f914aa782b1aabced
Nick Friskel <friskelglass@gmail.com>
2016-12-15 14:06:00 -0700

    3.06 trying to fix boot alarms

--- app/src/main/java/com/probationbuddy/probationbuddy/DayAlarm/DayAlarmOnBoot.java
+++ app/src/main/java/com/probationbuddy/probationbuddy/DayAlarm/DayAlarmOnBoot.java
@@ -5,21 +5,25 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.support.v7.preference.PreferenceManager;
+import android.util.Log;
 
 import com.probationbuddy.probationbuddy.MorningAlarm.MorningService;
 
 public class DayAlarmOnBoot extends WakefulBroadcastReceiver {
+    SharedPreferences sharedPrefs;
+    boolean dayAlarmActive;
+    Intent startServiceIntent;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-        boolean dayAlarmActive = sharedPrefs.getBoolean("dayAlarmRunning", true);
-
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        dayAlarmActive = sharedPrefs.getBoolean("dayAlarmRunning", true);
+        startServiceIntent = new Intent(context, MorningService.class);
 
         if (dayAlarmActive){
 
-            Intent startServiceIntent = new Intent(context, MorningService.class);
+            Log.i("BOOTY", "DayAlarmReceiverOnBoot is active");
             startWakefulService(context, startServiceIntent);
 
         }
--- app/src/main/java/com/probationbuddy/probationbuddy/MorningAlarm/MorningReceiverOnBoot.java
+++ app/src/main/java/com/probationbuddy/probationbuddy/MorningAlarm/MorningReceiverOnBoot.java
@@ -2,17 +2,29 @@ package com.probationbuddy.probationbuddy.MorningAlarm;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.content.WakefulBroadcastReceiver;
+import android.support.v7.preference.PreferenceManager;
+import android.util.Log;
 
 // WakefulBroadcastReceiver ensures the device does not go back to sleep
 // during the startup of the service
 public class MorningReceiverOnBoot extends WakefulBroadcastReceiver {
+    SharedPreferences sharedPrefs;
+    boolean alarmIsActive;
+    Intent startServiceIntent;
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        // Launch the specified service when this message is received
-        Intent startServiceIntent = new Intent(context, MorningServiceStarter.class);
-        startWakefulService(context, startServiceIntent);
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        alarmIsActive = sharedPrefs.getBoolean("prefsActivate", true);
+        startServiceIntent = new Intent(context, MorningServiceStarter.class);
+
+        if (alarmIsActive) {
+            Log.i("BOOTY", "MorningReceiverOnBoot is active");
+            startWakefulService(context, startServiceIntent);
+        }
     }
 }
 
