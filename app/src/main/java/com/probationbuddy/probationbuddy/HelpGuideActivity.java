7da36b0425007c8fa14b6f8759bbda0a94066405
Nick Friskel <friskelglass@gmail.com>
2016-12-16 18:24:36 -0700

    3.09 removed resetcalledtodaystarter service, not going to use

--- app/src/main/java/com/probationbuddy/probationbuddy/HelpGuideActivity.java
+++ app/src/main/java/com/probationbuddy/probationbuddy/HelpGuideActivity.java
@@ -2,6 +2,7 @@ package com.probationbuddy.probationbuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class HelpGuideActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class HelpGuideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_guide);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_top);
+        setSupportActionBar(myToolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+            getSupportActionBar().setDisplayUseLogoEnabled(true);
+        }
+        //set toolbar
+
+
     }
 }
